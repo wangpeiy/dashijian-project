be5cb1bd1418a11824a3cc95a7bd86ab6ab0448e
wangpeiyu <1227270970@qq.com>
2022-08-04 15:20:40 +0800

    基本资料完成

--- assets/js/user/userinfo.js
+++ assets/js/user/userinfo.js
@@ -22,25 +22,40 @@ const initUserInfo = () => {
 };
 
 // 重置表单数据
-$("#btnReset").click((e) => {
+$("#resetBtn").click((e) => {
   e.preventDefault();
   initUserInfo();
 });
 
-// 更新用户数据
-$(".layui-form").on("submit", (e) => {
+$('.layui-form').on('submit', function (e) {
   e.preventDefault();
   $.ajax({
-    type: "POST",
-    url: "/my/userinfo",
-    data: $(".layui-form").serialize(),
-    success: (res) => {
+    type: 'POST',
+    url: '/my/userinfo',
+    data: form.val('formUserInfo'),
+    success: res => {
       if (res.status !== 0) return layer.msg("更新用户信息失败！");
       layer.msg("更新用户信息成功！");
       // 调用父页面渲染函数
       window.parent.getUserInfo();
-    },
+    }
   });
 });
+
+// 更新用户数据
+// $(".layui-form").on("submit", (e) => {
+//   e.preventDefault();
+//   $.ajax({
+//     type: "POST",
+//     url: "/my/userinfo",
+//     data: $(".layui-form").serialize(),
+//     success: (res) => {
+//       if (res.status !== 0) return layer.msg("更新用户信息失败！");
+//       layer.msg("更新用户信息成功！");
+//       // 调用父页面渲染函数
+//       window.parent.getUserInfo();
+//     },
+//   });
+// });
 
 initUserInfo();
